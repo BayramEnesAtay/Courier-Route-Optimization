aeb6d782f95914e6989f8b56b66eb1bab9dc8acb
BayramEnesAtay <bayrameness4242@gmail.com>
2025-12-14 17:00:23 +0300

    Add:add some comment line

--- courier_route_optimization/src/App.jsx
+++ courier_route_optimization/src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import SignIn from "./components/signin/index.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/signup/index.jsx";
-import HeadlessDemo from "./components/sidebar/index.jsx";
+import SideBar from "./components/sidebar/index.jsx";
 
 const App=()=>{
-  
+  //the path for the sidebar will change.
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn/>}/> 
         <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/menu" element={<HeadlessDemo/>}/> 
+        <Route path="/menu" element={<SideBar/>}/> 
       </Routes>
     </BrowserRouter>
   );
